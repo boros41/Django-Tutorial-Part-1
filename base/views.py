358591a265feee4ecb6c07e21e164a6219ed4536
boros41 <kevstar421@gmail.com>
2025-02-02 23:53:48 -0500

    39:55 Templates timestamp complete (I prematurely pushed previous commit, next push should be Database & Admin Panel timestamp)

--- base/views.py
+++ base/views.py
@@ -3,10 +3,25 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+#  list of rooms containing a dictionary each containing an ID and name
+rooms = [
+    {"id":1, "name":"Leats learn python!"},
+    {"id":2, "name":"Design with me!"},
+    {"id":3, "name":"Frontend developers"},
+]
+
 # home page
 def home(request):
-    return HttpResponse("Home page")
+    context = {"rooms": rooms}
+    return render(request, 'base/home.html', context)
 
 # room page for different conversations
-def room(request):
-    return HttpResponse('ROOM') # return a specific page for a room
+def room(request, pk):
+    room = None # no room when we first go to link
+
+    for i in rooms:
+        if i["id"] == int(pk): # if the room ID matches the ID (pk) of the clicked link, that is our room
+            room = i
+
+    context = {"room": room}
+    return render(request, "base/room.html", context) # return a specific page for a room
