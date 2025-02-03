f036b8ac1eecfb5ae5a78f43bd9919af42f20196
boros41 <kevstar421@gmail.com>
2025-02-02 19:55:46 -0500

    39:55 - Templates timestamp completed

--- ProductName/urls.py
+++ ProductName/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # let urls file of base app handle all routing matching the route (empty string/core URL)
+    path('', include('base.urls'))
 ]
--- base/views.py
+++ base/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+# home page
+def home(request):
+    return HttpResponse("Home page")
+
+# room page for different conversations
+def room(request):
+    return HttpResponse('ROOM') # return a specific page for a room
