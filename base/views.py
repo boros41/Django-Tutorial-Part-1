100ee8c990a1d66d96d010bbf25d37779c1d90e3
boros41 <kevstar421@gmail.com>
2025-02-21 23:13:58 -0500

    2:39:03 - User Logout timestamp complete

--- base/views.py
+++ base/views.py
@@ -40,6 +40,10 @@ def loginPage(request):
     context = {}
     return render(request, 'base/login_register.html', context)
 
+def logoutUser(request):
+    logout(request) # deletes the token therefore deleting that user
+    return redirect("home")
+
 # home page
 def home(request):
     if request.GET.get("q") != None:
